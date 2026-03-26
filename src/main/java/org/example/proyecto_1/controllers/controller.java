14dc914c1e438c167471a922f615503901bb694e
SergioAnderson123 <a20223291@pucp.edu.pe>
2026-03-26 17:29:25 -0500

    feat: add UserController with GET /users endpoint

---
+++ src/main/java/org/example/proyecto_1/controllers/controller.java
@@ -0,0 +1,9 @@
+package org.example.proyecto_1.controllers;
+
+public class controller {
+    @org.springframework.web.bind.annotation.GetMapping("/")
+    public String index() {
+        return "Greetings from Spring Boot!";
+    }
+
+}
