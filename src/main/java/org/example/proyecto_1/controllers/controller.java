91a9f76d60b13ef12a3a7ec2798aaa00d0c04319
SergioAnderson123 <a20223291@pucp.edu.pe>
2026-03-26 17:54:01 -0500

    agregando gaa

--- src/main/java/org/example/proyecto_1/controllers/controller.java
+++ src/main/java/org/example/proyecto_1/controllers/controller.java
@@ -3,7 +3,7 @@ package org.example.proyecto_1.controllers;
 public class controller {
     @org.springframework.web.bind.annotation.GetMapping("/")
     public String index() {
-        return "Greetings from Spring Boot!";
+        return "Greetings from Spring Boot, gaaa!";
     }
 
 }
